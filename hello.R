341b7789ccdddbe7897d4d57e7d97892b5027362
Ryan Freer <ryanjfr@udel.edu>
2025-09-04 16:41:59 -0400

    test documents

---
+++ hello.R
@@ -0,0 +1,9 @@
+# R Script
+# Created: $(Get-Date -Format "yyyy-MM-dd")
+
+# Your R code goes here
+print("Hello, R!")
+
+# Example: Basic data analysis
+# data <- read.csv("your_data.csv")
+# summary(data)
